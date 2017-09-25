99e52bc12cbc2fb1e704ea564970950325e2e66f
Dustin Schroeder <dustin.p.schroeder@gmail.com>
2017-09-25 15:10:41 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var daysUntilMyBirthday = 60;
+
+for (var i = daysUntilMyBirthday; i>0; i--){
+    if(i>30){
+        console.log(i, "days until my birthday.  womp womp");
+    } else if(i>=6){
+        console.log("only", i,"days until my birthday!!");
+    } else if(i>1){
+        console.log(i, "DAYS UNTIL MY BIRTHDAYYYY!!!!");
+    } else{
+        console.log("1 DAY UNTIL MY BIRTHDAYYYY!!!!"); 
+    }
+}
+console.log("♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪ღ♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•*");
+console.log("♪ღ♪░H░A░P░P░Y░ B░I░R░T░H░D░A░Y░░♪ღ♪");
+console.log("*•♪ღ♪*•.¸¸¸.•*¨¨*•.¸¸¸.•*•♪¸.•*¨¨*•.¸¸¸.•*•♪ღ♪•«");
